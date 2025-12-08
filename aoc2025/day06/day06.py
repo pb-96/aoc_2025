9834fd91cb571014b6ba28ccb4301a7876f27431
pb-96 <philip.byrne@faculty.ai>
2025-12-08 16:08:38 +0000

    apply linting

--- aoc2025/day06/day06.py
+++ aoc2025/day06/day06.py
@@ -14,7 +14,7 @@ class DaySix(DayType):
     def parse_data_p1(self, data: list[str]):
         *rows, ops_line = data
         ops = ops_line.split()
-        
+
         columns = defaultdict(list)
         for row in rows:
             for col_idx, val in enumerate(row.split()):
@@ -28,7 +28,10 @@ class DaySix(DayType):
         ops = [c for c in ops_line if c in "*+"]
         delims.append(len(ops_line) + 1)
         chunks_by_row = [
-            [list(row[start:delims[i + 1] - 1].replace(" ", "0")) for i, start in enumerate(delims[:-1])]
+            [
+                list(row[start : delims[i + 1] - 1].replace(" ", "0"))
+                for i, start in enumerate(delims[:-1])
+            ]
             for row in rows
         ]
         columns = list(zip(*chunks_by_row))
@@ -46,7 +49,7 @@ class DaySix(DayType):
                 for idx in range(len(col) - 1, -1, -1):
                     if col[idx] != "0":
                         digit_buckets[idx].append(col[idx])
-            
+
             numbers = [int("".join(digits)) for digits in digit_buckets.values()]
             total += self.apply_op(op, numbers)
         return total
