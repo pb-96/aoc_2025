2481430c1ef4b4402ab4e71a918b31096aab065e
pb-96 <philip.byrne@faculty.ai>
2025-12-11 17:21:57 +0000

    cutting days

--- generate_boiler_plate.py
+++ generate_boiler_plate.py
@@ -13,19 +13,6 @@ NUMBER_TO_WORD = {
     10: "Ten",
     11: "Eleven",
     12: "Twelve",
-    13: "Thirteen",
-    14: "Fourteen",
-    15: "Fifteen",
-    16: "Sixteen",
-    17: "Seventeen",
-    18: "Eighteen",
-    19: "Nineteen",
-    20: "Twenty",
-    21: "TwentyOne",
-    22: "TwentyTwo",
-    23: "TwentyThree",
-    24: "TwentyFour",
-    25: "TwentyFive",
 }
 
 
